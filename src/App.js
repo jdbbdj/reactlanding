1aa444c49deb5f8a413be9666a7034d4b29536c1
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2021-09-10 14:39:52 +0800

    Updates

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Home from './pages';
+import SigninPage from './pages/signin';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+
+function App(){
+    return(
+        <Router className='App'>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/signin" component={SigninPage} exact/>
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
